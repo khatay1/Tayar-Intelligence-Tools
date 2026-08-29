1e83abfec96e3f83f4715871a363909130c658a4
khatay1 <khaled.m.tayar@gmail.com>
2026-08-29 11:54:07 +0200

    Document Admin context hook export

--- src/context/AdminContext.tsx
+++ src/context/AdminContext.tsx
@@ -63,6 +63,7 @@ export function AdminProvider({ children }: { children: ReactNode }) {
   );
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useAdmin() {
   const ctx = useContext(AdminContext);
 
