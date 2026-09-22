027d27977fef8206359f50b44ceeb39615ff69b6
khatay1 <khaled.m.tayar@gmail.com>
2026-09-22 17:03:57 +0200

    feat(builder): export Integrations MAX settings adapter

--- src/modules/website-builder/v2-ui/index.ts
+++ src/modules/website-builder/v2-ui/index.ts
@@ -19,6 +19,7 @@ export * from './BuilderCanvasMeasurement';
 export * from './CanvasSpacingGuide';
 export * from './BuilderDropIndicator';
 export * from './BuilderIntegrationsMaxPanel';
+export * from './BuilderIntegrationsSettingsSlot';
 
 export * from './WebsiteBuilderV2Bridge';
 
