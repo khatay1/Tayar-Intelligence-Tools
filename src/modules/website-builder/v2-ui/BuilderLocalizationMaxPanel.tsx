37b415271afff990330a9fe6406c42d66427ae8a
khatay1 <khaled.m.tayar@gmail.com>
2026-09-22 16:24:29 +0200

    Wire Localization MAX translations

--- src/modules/website-builder/v2-ui/BuilderLocalizationMaxPanel.tsx
+++ src/modules/website-builder/v2-ui/BuilderLocalizationMaxPanel.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from 'react';
-import { useLocalizer } from '@/lib/ui-localization';
+import { useLocalizer } from '@/lib/ui-localization-localization-max';
 import type { EditorPageLike } from '../core/editor-model';
 import type { EditorLocalizationConfig, EditorLocaleDefinition, EditorLocalizedPageContent } from '../core/editor-localization';
 import { buildEditorLocalizedPath, resolveEditorLocalizedPageContent } from '../core/editor-localization';
